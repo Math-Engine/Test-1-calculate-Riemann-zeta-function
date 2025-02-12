2a4296eba6830e6faa2745165d717e532e475d0b
happy-spark <107300540+happy-spark@users.noreply.github.com>
2025-02-12 15:47:42 +0900

    getcontext().prec = 1000

--- calculate-PI-squared.py
+++ calculate-PI-squared.py
@@ -11,18 +11,19 @@ with open('denominator.txt', 'r') as f:
 with open('numerator.txt', 'r') as f:
   numerator = int(f.read().strip())
 
-getcontext().prec = 2147483647
+getcontext().prec = 1000
 
 fraction = Fraction(numerator * 6, denominator)
 
 decimal_value = Decimal(fraction.numerator) / Decimal(fraction.denominator)
 
-print("============================= Happy BirthDay GitHub!!! =============================")
-print("============================= 오늘은 GitHub 출시 17주년입니다. =============================")
-print("============================= Happy BirthDay GitHub!!! =============================")
+# print("============================= Happy BirthDay GitHub!!! =============================")
+# print("============================= 오늘은 GitHub 출시 17주년입니다. =============================")
+# print("============================= Happy BirthDay GitHub!!! =============================")
 
-print("\n\n")
+# print("\n\n")
 
 print("==========================================================================================")
+print("\n\n")
 
 print(decimal_value)
